f69f898c31ebbc5af7af3b6953e55d1b01134103
Zsolt Nagy <info@zsoltnagy.eu>
2020-06-01 02:14:26 +0200

    lifting state up

--- src/App.js
+++ src/App.js
@@ -44,13 +44,30 @@ class App extends React.Component {
         },                       
       ]
     }
+    this.handleRefresh = this.handleRefresh.bind(this);
+  }
+  handleRefresh(valueChangeTicker) {
+    const newCoinData = this.state.coinData.map( function( {ticker, name, price} ) {
+      let newPrice = price;
+      if ( valueChangeTicker === ticker ) {
+        const randomPercentage = 0.995 + Math.random() * 0.01;
+        newPrice = newPrice * randomPercentage;
+      }
+      return {
+        ticker,
+        name,
+        price: newPrice
+      }
+    });
+    
+    this.setState({ coinData: newCoinData });
   }
   render() {
     return (
       <Div className="App">
         <ExchangeHeader />
         <AccountBalance amount={this.state.balance} />
-        <CoinList coinData={this.state.coinData} />
+        <CoinList coinData={this.state.coinData} handleRefresh={this.handleRefresh} />
       </Div>
     );
   }
--- src/components/Coin/Coin.jsx
+++ src/components/Coin/Coin.jsx
@@ -10,28 +10,30 @@ const Td = styled.td`
 export default class Coin extends Component {
     constructor(props) {
         super(props); 
-        this.state = {
-            price: this.props.price
-        }
         this.handleClick = this.handleClick.bind(this);
     }
+    
     handleClick(event) {
         // Prevent the default action of submitting the form 
         event.preventDefault();
 
+        this.props.handleRefresh(this.props.ticker);
+/*
         const randomPercentage = 0.995 + Math.random() * 0.01;
         this.setState( function(oldState) {
             return {
                 price: oldState.price * randomPercentage
             };
-        });        
+        });    
+*/    
     }
+    
     render() {
         return (
             <tr> 
               <Td>{this.props.name}</Td>
               <Td>{this.props.ticker}</Td> 
-              <Td>${this.state.price}</Td>
+              <Td>${this.props.price}</Td>
               <Td>
                   <form action="#" method="POST">
                     <button onClick={this.handleClick}>Refresh</button>
