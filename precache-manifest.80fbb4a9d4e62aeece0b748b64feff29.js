151d9e31398e471f781eb40ee9d365b46041d650
Zsolt Nagy <info@zsoltnagy.eu>
2020-06-07 13:35:21 +0200

    Updates

---
+++ precache-manifest.80fbb4a9d4e62aeece0b748b64feff29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b42deb94bf7278dc77db385e6f8f6e01",
+    "url": "/coin-exchange/index.html"
+  },
+  {
+    "revision": "1871072bd5bfb3e81879",
+    "url": "/coin-exchange/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "74c85ff2271280590df3",
+    "url": "/coin-exchange/static/js/2.29c2c344.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/coin-exchange/static/js/2.29c2c344.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1871072bd5bfb3e81879",
+    "url": "/coin-exchange/static/js/main.39b3ebec.chunk.js"
+  },
+  {
+    "revision": "8d74aa542016e19b2bdf",
+    "url": "/coin-exchange/static/js/runtime-main.075daf70.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/coin-exchange/static/media/logo.5d5d9eef.svg"
+  }
+]);
